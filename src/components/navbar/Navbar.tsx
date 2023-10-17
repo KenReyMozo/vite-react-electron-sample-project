528f07a7640c3dc806e57f95d31b35cb29cea632
KendallRey <107897863+KendallRey@users.noreply.github.com>
2023-10-17 21:22:12 +0800

    added update profile

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import style from './Navbar.module.scss';
 import { User } from '@firebase/auth';
+import { useNavigate } from 'react-router-dom';
 
 type NavbarType = {
   user: User;
@@ -10,10 +11,16 @@ type NavbarType = {
 const Navbar: React.FC<NavbarType> = (props) => {
   const { user, children } = props;
 
+  const navigate = useNavigate()
+
+  const onClickProfile = async () => {
+    navigate('/user/profile')
+  }
+
   return (
     <div className={`${style.container} bg-gray-600`}>
-      <div className="flex items-center text-white">
-        <img src="/vite.svg" className={style.profile_picture} />
+      <div className={style.profile} onClick={onClickProfile}>
+        <img src="/vite.svg"/>
         <span className="ml-2">{user.displayName ?? '...'}</span>
       </div>
       {children}
--- src/pages/profile-page/ProfilePage.tsx
+++ src/pages/profile-page/ProfilePage.tsx
@@ -1,14 +1,34 @@
 import useFirebaseAuth from '@/firebase-manager/auth/AuthHook';
 import style from './ProfilePage.module.scss';
 import Input from '@/components/input/Input';
+import { useState } from 'react';
+import { User, updateProfile } from 'firebase/auth';
 
 const ProfilePage: React.FC = () => {
+
   const { user } = useFirebaseAuth({ fallback_to: '/' });
+
+  const [userProfile, setUserProfile] = useState({
+    displayName : '',
+  })
+
+  const onChangeUserProfile = (e : RCE<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setUserProfile(prev => ({...prev, [name] : value}))
+  }
+
+  const onUpdateProfile = async () => {
+    if(!user) return;
+    await updateProfile(user, userProfile)
+  }
 
   return (
     <div className={style.container}>
-      <div className="grid gap-6 mb-6 md:grid-cols-2">
-        <Input label="Name" />
+      <div className="m-4 grid gap-6 mb-6 md:grid-cols-2">
+        <Input label="Name" required
+          onChange={onChangeUserProfile}
+          name='displayName'/>
+        <button onClick={onUpdateProfile}>Update</button>
       </div>
     </div>
   );
